421430a176cc54f31938d5096aee1e2280f0a46a
hedgeday <sachin.hegde91@gmail.com>
2012-11-20 21:23:00 -0500

    fixed everything

--- server/server.js
+++ server/server.js
@@ -1,15 +1,3 @@
-// // server.js
-// // a very simple node server using the express module
-// var express = require("express");
-// var app = express();
-// var wwwDir = "/www";
-// app.use("/", express.static(__dirname + wwwDir));
-// app.get("/", function(req, res) { res.render(wwwDir + "/index.html");});
-// app.listen(8080);
-
-
-//END OF OLD FILE
-
 // simpleExpressServer.js
 // A simple Express server for 15-237.
 
